c0587f2d2695e5cd0545d5e83638d1a9b2c102ed
Jinkyeong Yun <oo87o425@naver.com>
2022-03-07 02:11:21 +0900

    jk1

---
+++ camper/src/main/java/com/camper/model/NboardTO.java
@@ -0,0 +1,17 @@
+package com.camper.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class NboardTO {
+	private String nseq;
+	private String title;
+	private String nick;
+	private String content;
+	private String img;
+	private String type;
+	private String wdate;
+	
+}
